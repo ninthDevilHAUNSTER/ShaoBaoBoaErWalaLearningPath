d839af15ddefdd673d18d1e6f56b46f3e065480e
ninthDevilHAUNSTER <luotianhan@126.com>
2022-10-13 17:14:10 +0800

    Update some contents

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,92 @@
 package org.example;
 
 
+import com.ibm.wala.cast.java.client.impl.ZeroOneCFABuilderFactory;
+import com.ibm.wala.cast.types.AstMethodReference;
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.classLoader.Language;
+import com.ibm.wala.client.AbstractAnalysisEngine;
 import com.ibm.wala.core.util.config.AnalysisScopeReader;
 import com.ibm.wala.ipa.callgraph.*;
+import com.ibm.wala.ipa.callgraph.impl.AllApplicationEntrypoints;
 import com.ibm.wala.ipa.callgraph.impl.Everywhere;
 import com.ibm.wala.ipa.callgraph.impl.Util;
 import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
+import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis;
+import com.ibm.wala.ipa.callgraph.util.CallGraphSearchUtil;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.ipa.slicer.Slicer;
+import com.ibm.wala.ipa.slicer.SlicerUtil;
+import com.ibm.wala.ipa.slicer.Statement;
 import com.ibm.wala.ssa.*;
 import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.types.Selector;
+import com.ibm.wala.util.CancelException;
 
+import javax.swing.plaf.nimbus.State;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Iterator;
 
 
 public class Main {
     public static void main(String[] args) {
-        learnIR();
+        learnSlice();
+    }
+
+    public static void learnSlice() {
+        try {
+            AnalysisScope scope = AnalysisScopeReader.instance.makeJavaBinaryAnalysisScope(
+                    "src/main/resources/com.ibm.wala.core.testdata_1.0.0.jar", new File("exclusion.txt")
+            );
+            IClassHierarchy cha = ClassHierarchyFactory.make(scope);
+            // debug for all classes will be analyzed.
+            for (IClass iClass : cha
+            ) {
+                if (iClass.getClassLoader().getReference().equals(ClassLoaderReference.Application)) {
+                    System.out.println(iClass);
+                }
+            }
+            // generate options
+
+            // generate entry point
+            // Lslice represent for package
+            Iterable<Entrypoint> entrypoints =
+                    com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(cha, "Lslice/Slice1");
+            AnalysisOptions options = new AnalysisOptions(scope, entrypoints);
+
+            // build call graph
+            CallGraphBuilder<InstanceKey> builder = new ZeroOneCFABuilderFactory().make(new AnalysisOptions(scope, entrypoints), new AnalysisCacheImpl(), cha);
+            CallGraph cg = builder.makeCallGraph(options, null);
+            CGNode main = CallGraphSearchUtil.findMainMethod(cg);
+
+            Statement s = SlicerUtil.findCallTo(main, "println");
+            System.out.println("Statement : " + s);
+
+            // calcluate the param of println function
+            PointerAnalysis<InstanceKey> pointerAnalysis = builder.getPointerAnalysis();
+            Collection<Statement> statementCollection = Slicer.computeBackwardSlice(
+                    s, cg, pointerAnalysis, Slicer.DataDependenceOptions.FULL, Slicer.ControlDependenceOptions.NONE
+            );
+             SlicerUtil.dumpSlice(statementCollection);
+            int i = 0;
+            for (Statement statement :statementCollection
+                    ) {
+                System.out.println(statement.getNode());
+            }
+
+
+        } catch (IOException | ClassHierarchyException e) {
+            throw new RuntimeException(e);
+        } catch (CallGraphBuilderCancelException e) {
+            throw new RuntimeException(e);
+        } catch (CancelException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void learnIR() {
@@ -87,7 +150,6 @@ public class Main {
 //                    Language.JAVA, o, new AnalysisCacheImpl(), cha, scope
 //            );
 //            CallGraph cg = builder.makeCallGraph(o, null);
-//            // 如上过程能够顺利建立一个jar包的cg，后续API在后续分析中使用。
 //            System.out.println("Construction Finished");
         } catch (IOException e) {
             throw new RuntimeException(e);
